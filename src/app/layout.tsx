d42b57e80f6d29f19e452fdaa7e6276092a8eeac
Juan Alarcon <juan.s.alarcondiaz@gmail.com>
2025-02-08 00:05:23 -0500

    PD-5 chore: Update project configuration files
    Replaced `.prettierrc` with `pretier.config.mjs` for best practices.
    Adjust Jest setup for tests
    Update tailwind.config.ts for more styles
    Add paths to tsconfig.json
    Next.js configuration for svgr compatibility
    Update package.json scripts and dev dependences (test and eslint version)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { NextFont } from "next/dist/compiled/@next/font";
+import { Toaster } from "@/components/ui/toaster";
+import Auth from "@/app/auth";
 
 const montserrat: NextFont = Montserrat({
   subsets: ["latin"],
@@ -20,8 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className={montserrat.className}>
-      <body className="bg-jaguar-950 px-6 pt-32">{children}</body>
+    <html lang="es" className={`scroll-smooth ${montserrat.className}`}>
+      <Auth>
+        <body className="bg-jaguar-950 px-6 pt-32 data-[scroll-locked]:pt-32">
+        {children}
+        <Toaster />
+        </body>
+      </Auth>
     </html>
   );
 }
